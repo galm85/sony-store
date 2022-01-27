2e2e5f7470b10cf795ec3320f91f509c284745d5
galm85 <galm85@gmail.com>
2022-01-27 17:23:02 +0200

    update messages routes and model

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -4,8 +4,10 @@ const messageSchema = new mongoose.Schema({
     name:String,
     phone:String,
     email:String,
+    subject:String,
     message:String,
-    status:String
+    status:String,
+    read:Boolean,
 },{timestamps:true})
 
 const Message = mongoose.model('Message',messageSchema);
--- routes/messagesRoute.js
+++ routes/messagesRoute.js
@@ -1,25 +1,74 @@
 const router = require('express').Router();
-const { findByIdAndUpdate } = require('../models/messagesModel');
 const Message = require('../models/messagesModel');
 
 
 //get all messages
 router.get('/',async(req,res)=>{
-    const messages = await Message.find({});
-    res.status(200).send(messages);
+    try{
+        const messages = await Message.find({}).sort({createdAt:-1});
+        res.status(200).send(messages);
+
+    }catch(err){
+        return res.status(400).send(err);
+    }
 })
 
 
+//post new message
 router.post('/',async(req,res)=>{
-    const message = new Message(req.body);
-    message.status = 'unread';
-    await message.save();
-    res.status(200).send('Thank for your message');
+    try{
+
+        const message = new Message(req.body);
+        message.read = false;
+        message.status = 'new';
+        await message.save();
+        return res.status(200).send('Thank for your message');
+    }catch(err){
+        return res.status(400).send(err);
+    }
 })
 
-router.patch('/update-status/:messageId',async(req,res)=>{
-    const message = await Message.findByIdAndUpdate(req.params.messageId,{status:req.body.status});
+
+// delete Message by Id
+router.delete('/delete-message/:messageId',async(req,res)=>{
+    try {
+        await Message.findByIdAndRemove(req.params.messageId);
+        return res.status(200).send('Message Deleted');
+    } catch (error) {
+        return res.status(400).send(err);
+    }
+})
+
+
+router.patch('/update-status/:messageId/:status',async(req,res)=>{
+    const message = await Message.findByIdAndUpdate(req.params.messageId,{status:req.params.status});
     res.send('message status update');
+
+})
+
+router.patch('/read-message/:messageId/:read',async(req,res)=>{
+    
+    let read;
+    if(req.params.read == 1){
+        read = true;
+    }else{
+        read = false;
+    }   
+    const messageId = req.params.messageId;
+    await Message.findByIdAndUpdate(messageId,{read:read});
+    return res.status(200);
+    
+})
+
+
+
+router.get('/search/:email',async(req,res)=>{
+    try{
+        const messages = await Message.find({email:{ $regex: new RegExp("^" + req.params.email.toLowerCase(), "i") } });
+        return res.status(200).send(messages);
+    }catch(err){
+        res.status(400).send(err);
+    }
 
 })
 
